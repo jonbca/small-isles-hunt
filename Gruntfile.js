3e25b8987ff011c9650dcb3fb5384025e3d9dd45
Jonathan Abourbih <jon.abourbih@gmail.com>
2013-07-13 18:18:48 +0100

    Starting work on hunting!

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,29 @@
+var child;
+
 module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         watch: {
             files: ['public/**/*'],
-            tasks: ['default'],
-            options: {
-                livereload: 35729
-            }
-        },
-        browserify: {
-            'build/scripts/game.js': ['public/javascripts/game.coffee'],
-            options: {
-                transform: ['coffeeify'],
-                aliasMappings: [
-                    {
-                        cwd: 'public/javascripts',
-                        src: ['**/*.coffee'],
-                        dest: '',
-                        flatten: true
-                    }
-                ]
-            }
-        },
-        copy: {
-            main: {
-                files: [
-                    {
-                        expand: true,
-                        src: ['**/*'],
-                        dest: 'build/styles',
-                        cwd: 'public/stylesheets'
-                    },
-                    {
-                        expand: true,
-                        src: ['images/**/*', 'index.html'],
-                        dest: 'build',
-                        cwd: 'public'
-                    }
-                ]
-            }
+            tasks: ['default', 'start'],
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-browserify');
 
-    grunt.registerTask('default', ['browserify', 'copy']);
-    grunt.registerTask('develop', ['default', 'watch']);
+    grunt.registerTask('start', function () {
+        if (child) {
+            child.kill();
+        }
+
+        child = grunt.util.spawn({
+            cmd: 'node',
+            args: ['app.js'],
+            opts: {
+                stdio: 'inherit'
+            }
+        });
+    });
+
+    grunt.registerTask('default', ['start', 'watch']);
 };
